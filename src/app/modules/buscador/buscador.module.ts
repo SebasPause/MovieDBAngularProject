ef651762417aa157e4d1b258831da2384b171b87
Sebastian <sebastianpauseniuc@gmail.com>
2021-11-20 17:47:19 +0100

    Creo los módulos de "peliculas" y "buscador"

---
+++ src/app/modules/buscador/buscador.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BuscadorRoutingModule } from './buscador-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BuscadorRoutingModule
+  ]
+})
+export class BuscadorModule { }
