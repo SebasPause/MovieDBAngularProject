11979a706143d1ee6eb20423f54d2fa2069b9402
Sebastian <sebastianpauseniuc@gmail.com>
2021-11-25 21:13:32 +0100

    Modifico el servicio de peliculas  para obtener una lista de peliculas

--- src/app/modules/peliculas/peliculas.module.ts
+++ src/app/modules/peliculas/peliculas.module.ts
@@ -6,6 +6,7 @@ import { PeliculaComponent } from './lista-peliculas/pelicula/pelicula.component
 
 
 
+
 @NgModule({
   declarations: [
     ListaPeliculasComponent,
--- src/app/modules/peliculas/service/pelicula-service.service.ts
+++ src/app/modules/peliculas/service/pelicula-service.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { listaPeliculas } from '../models/listaPeliculas.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculaServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  public GetAll():Observable<listaPeliculas> {
+    return this.http.get<listaPeliculas>('https://developers.themoviedb.org/3/movies/get-movie-lists');
+  }
+
 }
+
+
+
