ce97b03c406666b74a146f56eacec434190a0315
Sebastian <sebastianpauseniuc@gmail.com>
2021-11-27 20:58:53 +0100

    Creo el modelo "genero"

--- src/app/modules/peliculas/service/pelicula-service.service.ts
+++ src/app/modules/peliculas/service/pelicula-service.service.ts
@@ -16,6 +16,7 @@ export class PeliculaServiceService {
     return this.http.get<listaPeliculas>('http://api.themoviedb.org/3/discover/movie?api_key=7d1e614246e33b9b52f42a293ca206f7');
   }
 
+  
 
 }
 
