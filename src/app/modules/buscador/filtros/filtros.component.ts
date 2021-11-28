64c15b781a5e1024aa60c9fa5ad5fd9f089c9873
Sebastian <sebastianpauseniuc@gmail.com>
2021-11-29 00:06:49 +0100

    Agrego función de input para detectar cuando cambia el contenido del input

--- src/app/modules/buscador/filtros/filtros.component.ts
+++ src/app/modules/buscador/filtros/filtros.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FiltrosComponent implements OnInit {
 
+  titulo: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  cambiarBusqueda(event: any,filtro: string): void{
+    
   }
 
 }
